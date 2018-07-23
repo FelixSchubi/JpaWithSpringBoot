67c04c0c3238ed75d60983442b0cdb49e85472c0
FelixSchubi <felix.mammendorf@live.de>
2018-07-23 11:34:02 +0200

    First_>init

---
+++ src/main/java/org/hameister/personmanager/model/Person.java
@@ -0,0 +1,60 @@
+package org.hameister.personmanager.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "Person")
+public class Person {
+
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @Column(name = "name")
+    String name;
+
+
+    @Column(name = "salary")
+    Long salary;
+
+
+    @Column(name = "birthday")
+    LocalDate birthday;
+
+    public Person() {
+    }
+
+    public Long getId() {
+    return id;
+    }
+
+    public void setId(Long id) {
+    this.id = id;
+    }
+
+    public String getName() {
+    return name;
+    }
+
+    public void setName(String name) {
+    this.name = name;
+    }
+
+    public Long getSalary() {
+    return salary;
+    }
+
+    public void setSalary(Long salary) {
+    this.salary = salary;
+    }
+
+    public LocalDate getBirthday() {
+    return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+    this.birthday = birthday;
+    }
+}
